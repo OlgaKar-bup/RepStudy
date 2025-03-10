d9bd6ac148c370f781c0e77ccaba57d3cf40743f
OlgaKar <karkusha@gmail.com>
2025-03-10 15:42:35 +0300

    Move the name code into its own function and file.

--- main.c
+++ main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+
+#include "askname.h"
 
 int main(int argc, char **argv)
 {
-    char name[255];
+    char first[255], last[255];
 
-     printf("Enter your first name: ");
-     fgets(first, 255, stdin);
-     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
-     printf("length = %d\n", strlen(name)); /* debug line */
-     printf("Enter your first name: ");
-     fgets(first, 255, stdin);
-     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+    askname(first, last);
 
-    printf("Hello, %s!\n", first, last);
+    printf("Hello, %s %s!\n", first, last);
     return 0;
 }
